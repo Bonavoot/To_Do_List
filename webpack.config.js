503215c6fbeaa2cf4e31f1cfccf1bb5032d71285
Joey Bonventre <Jbonventre95@gmail.com>
2022-03-17 07:50:55 -0400

    beginning stage complete

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ToDoList",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
