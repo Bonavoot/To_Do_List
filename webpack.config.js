4510e99f06cbea3c8033f9fc20a56b54b58d54c2
Joey Bonventre <Jbonventre95@gmail.com>
2022-03-18 20:36:20 -0400

    preliminary layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: {
-    main: path.resolve(__dirname, "./src/index.js"),
+    index: "./src/index.js",
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "ToDoList",
-    }),
-  ],
+  devServer: {
+    static: "./dist",
+  },
+
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
